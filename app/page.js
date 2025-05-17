63f79f04ad6722e91a7b5e00b73feb18c1861e21
MahdMalik <mahdmahdmalik@gmail.com>
2025-05-17 14:33:00 -0500

    doing first push of code

--- app/page.js
+++ app/page.js
@@ -1,103 +1,136 @@
+'use client'
+
 import Image from "next/image";
+import { Button } from "@mui/material";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [hasError, setError] = useState(false)
+  const [errMessage, setErrMessage] = useState("")
+  const [chubFileName, setChubName] = useState("")
+  const [chubData, setChubData] = useState("")
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const downloadFile = async() => {
+    const blob = new Blob([chubData], {type: "text/plain"})
+    const url = URL.createObjectURL(blob)
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = chubFileName;
+
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+
+    URL.revokeObjectURL(url);
+  }
+
+  const readFunct = async(file) => 
+  {
+    return new Promise((resolve, reject) => 
+    {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        resolve(reader.result); // resolves with the file content
+      };
+
+      reader.onerror = () => {
+        reject(reader.error); // rejects with the error
+      };
+
+      reader.readAsText(file);
+    });
+  }
+  
+  const convertData = async(event) => {
+    const file = event.target.files[0]
+
+    if(!file)
+    {
+      setErrMessage("Error converting: seems I can't open the file.")
+      setError(true);
+      return;
+    }
+
+    //first split by periods, get the last line after the period to get hte extension
+    const extension = file.name.split(".").pop().toLowerCase();
+    if(extension != "json")
+    {
+      setErrMessage("Plz submit a valid json file")
+      setError(true)
+      return
+    }
+
+    setChubName("CHUB_" + file.name)
+
+    let text = ""
+    try
+    {
+      text = await readFunct(file);
+    }
+    catch(error)
+    {
+      setErrMessage("Error parsing the file: " + error)
+      return;
+    }
+
+    //this is the xoul parsed and stuff into an object. Now the fun begins!
+    const xoulJson = JSON.parse(text);
+    console.log(xoulJson);
+
+    let chubData = {}
+
+    let chubJson = {spec: "chara_card_v2", spec_version: "2.0", data: chubData};
+     
+    chubData.name = xoulJson.name
+    //chub combines both these sectinons
+    chubData.description = xoulJson.backstory + "\n\n\n" + xoulJson.definition
+    chubData.first_mes = xoulJson.greeting
+    chubData.mes_example = xoulJson.samples
+    //I think this is right?
+    chubData.creator_notes = xoulJson.bio
+    //this may not end up returning the correct corresponding tags, so you'll
+    //likely have to re-input your tags
+    chubData.tags = xoulJson.social_tags
+
+    //lets hope this works lol
+    chubData.tagline = xoulJson.tagline
+
+    console.log(chubJson)
+
+    //convert this back into json format
+    chubJson = JSON.stringify(chubJson)
+
+    console.log(chubJson)
+
+
+    setError(false)
+    setErrMessage("")
+
+    setChubData(chubJson)
+
+    console.log("file name: " + file.name);
+  }
+  
+  return (
+    <div className="grid items-center justify-items-center">
+      <p>NOTE: you will have to put in the tagline and creator's notes yourself; as far as I know, Chub can't let you import that</p>
+      <br></br>
+      <p>Currently this can convert xouls and lorebooks to the right format. Apologies for the bad UI I don't like doing frontend stuff.</p>
+      <br></br>
+      <p>Drop Your Json File Here:</p>
+      <label className="bg-blue-600 text-white px-4 py-2 rounded cursor-pointer hover:bg-blue-700">
+        Upload File
+        <input type="file" accept=".json" className="hidden" onChange={convertData}/>
+      </label>
+      <br></br>
+      {hasError ? (<p>ERROR: {errMessage}</p>) : (<p></p>)}
+      {chubData != "" ? (
+        <div>
+          <p>File Ready! Click here To Download:</p>
+          <Button variant="contained" color="error" onClick={downloadFile}>Download New File For Chub</Button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        ) : (<p></p>)}
     </div>
   );
 }
